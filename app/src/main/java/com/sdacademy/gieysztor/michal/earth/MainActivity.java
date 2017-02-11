8dae0f17e0f29f0c47199cb423ccbd403b7764ce
mgieysztor <michal.gieysztor@gmail.com>
2017-02-11 11:23:09 +0100

    Initial commit

---
+++ app/src/main/java/com/sdacademy/gieysztor/michal/earth/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sdacademy.gieysztor.michal.earth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
